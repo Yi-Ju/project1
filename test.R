efa78d6a4f27389cac465bfc1ebc68d2f3301e50
Yi-Ju <jirhrs7@gmail.com>
2018-10-12 16:36:00 +0800

    add workflow on 16:35

--- test.R
+++ test.R
@@ -2,3 +2,32 @@
 # I add this line on GitHub website. 15:42
 
 ## after I click the Pull button, the line above (15:42) appears. and this line is typed on 15:51
+
+## ================##
+# step by step work flow of GitHub and Rstudio
+
+# 1: (GitHub)
+    # create a new repository (eg., project1) on GitHub
+    # click Clone > copy URL
+
+# 2: (Rstudio)
+    # New project > Version Control > Git > paste URL, create project as subdirectory of: ~/ Google drive
+    # The R project called project1
+    # New Script (eg., test.R) > edit code > save
+
+# 3: (Rstudio)
+    # Git button in 'Environment, History...'
+    # Staged > check the checkboxs of files I want to update > Commit > write Commit message > commit > push
+
+# 4: (GitHub)
+    # reload/ update the GitHub website, the new file (test.R) appears.
+
+# 5: (GitHub)
+    # Edit file directly on GitHub, add new line on GitHub website 
+
+# 6: (Rstudio)
+    # Reopen project1 > 'Git button' > Pull
+    # editing and save codes.
+    # move to step 3.
+
+
