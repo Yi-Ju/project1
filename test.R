bdc95fe1a05d9b24b5e61197ac87bff9ad271bc7
Yi-Ju <jirhrs7@gmail.com>
2018-10-12 15:52:33 +0800

    15:51 from Rsudio

--- test.R
+++ test.R
@@ -1,2 +1,4 @@
 # Oct. 12 15:36 I type this line on Rsudio, plan to push this script to github later.
 # I add this line on GitHub website. 15:42
+
+## after I click the Pull button, the line above (15:42) appears. and this line is typed on 15:51
